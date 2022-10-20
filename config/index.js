f3bb871fc6e666d49399f67fcd58e452ef5ed0c3
“naandalist” <listiananda.apriliawan@gmail.com>
2022-10-21 01:52:59 +0700

    feat: add gatsby configuration

---
+++ config/index.js
@@ -0,0 +1,33 @@
+module.exports = {
+  title: "Naandalist's Blog",
+  description:
+    "This is a Naandalist's personal homepage and blog with GatsbyJs ",
+  author: "Naandalist",
+  twitterUsername: "@bunzo",
+  keywords: [
+    "naandalist",
+    "listiananda",
+    "bootstrap",
+    "html",
+    "react",
+    "gatsby",
+    "homepage",
+    "blog",
+    "creative",
+  ],
+  //image: "banner_image.png",
+  siteUrl: "https://naandalist.dev",
+  pathPrefix: "/blog",
+  localUrl: "http://localhost:8002",
+  canonical: "https://naandalist.dev",
+  social: {
+    facebook: "https://www.facebook.com/naandalist",
+    twitter: "https://twitter.com/naandalist",
+    linkedin: "https://www.linkedin.com/listiananda-apriliawan",
+  },
+  siteLanguage: ["en", "id"],
+  favicon: "src/assets/images/favicon.png", // Used for manifest favicon generation
+  shortName: "naandalist", // shortname for manifest. *Must* be shorter than 12 characters
+  themeColor: "#5E60E7",
+  backgroundColor: "#fff",
+};
